0bcc316868790b08935b5c91ca6e69ef6366801a
Lforlinux <karthik.kln@gmail.com>
2021-01-30 17:13:00 +0530

    Update terraform-install.sh

--- scripts/terraform-install.sh
+++ scripts/terraform-install.sh
@@ -10,8 +10,8 @@ if [ $status -eq 0 ]; then
   echo "terraform  installed already, Skipping this stage"
   exit 0 
   else 
-  sudo wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
-  sudo unzip terraform_0.14.5_linux_amd64.zip
+  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
+  unzip terraform_0.14.5_linux_amd64.zip
   sudo mv terraform /bin/
   terraform -v
   rm -rf terraform_0.14.5_linux_amd64.zip
