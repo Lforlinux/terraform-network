598566f22dade8db7b5ca5456f772c29ae4f93be
Lforlinux <karthik.kln@gmail.com>
2021-01-30 17:07:08 +0530

    Update terraform-init.sh

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -11,8 +11,8 @@ if [ $status -eq 0 ]; then
   terraform init
   exit 0 
   else 
-  wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.14.5_linux_amd64.zip
-  unzip terraform_0.14.5_linux_amd64.zip
+  sudo wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.14.5_linux_amd64.zip
+  sudo unzip terraform_0.14.5_linux_amd64.zip
   sudo mv terraform /bin/
   terraform -v
   rm -rf terraform_0.14.5_linux_amd64.zip
