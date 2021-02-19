fba325c4c7cf2898c316aa36cf5b41ccab4ddbd7
Lforlinux <karthik.kln@gmail.com>
2021-02-19 21:32:04 +0530

    Update terraform-init.sh

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-which terraform && terraform --version
-
-# Get the status
-status=$?
-
-#terraform init 
-if [ $status -eq 0 ]; then
-  echo "terraform  installed already, proceeding terraform init"
-  terraform init
-  exit 0 
-  else 
-  sudo wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
-  sudo unzip terraform_0.14.5_linux_amd64.zip
-  sudo mv terraform /bin/
-  terraform -v
-  rm -rf terraform_0.14.5_linux_amd64.zip
-  exit  $status
-fi
+docker run -it --rm lforlinux/terraform:0.14.5 init
 
