c8fc9c301fdd696787c2177a251be85c47902e02
KLN <karthik.kln@gmail.com>
2021-08-06 16:27:30 +0530

    adding sudo

--- scripts/terraform-bin-install.sh
+++ scripts/terraform-bin-install.sh
@@ -10,7 +10,7 @@ if [ $status -eq 0 ]; then
   exit 0
   else
   rm terraform
-  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip && unzip terraform_0.14.5_linux_amd64.zip && mv -f terraform /bin/
+  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip && unzip terraform_0.14.5_linux_amd64.zip && sudo mv -f terraform /bin/
   terraform -v
   rm -rf terraform_0.14.5_linux_amd64.zip
   exit  $status
