d50b36280de49b8c2bbeded68b592e2b3c4bf719
KLN <karthik.kln@gmail.com>
2021-08-06 16:08:18 +0530

    new code added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # terraform-network
+install terraform
+```
+wget https://releases.hashicorp.com/terraform/1.0.4/terraform_1.0.4_linux_amd64.zip
+unzip terraform_1.0.4_linux_amd64.zip
+cp terraform /usr/bin/
+terraform  -v
+```
--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-docker run -it --rm lforlinux/terraform:0.14.5 init
+#docker run -it --rm lforlinux/terraform:0.14.5 init
 
+
+#!/bin/bash
+
+which terraform
+
+# Get the status
+status=$?
+
+#terraform planning
+if [ $status -eq 0 ]; then
+  echo "terraform  found"
+  export ${AWS_ACCESS_KEY}
+  export ${AWS_SECRET_ACCESS_KEY}
+  echo ${AWS_ACCESS_KEY}
+  #cat /dev/zero | ssh-keygen -q -N "" ; ls -al $HOME/.ssh/id_rsa.pub
+  terraform init 
+  else 
+  echo "Not able to perform the plan"
+fi
