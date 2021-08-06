e05bcc0f9cb24738b606587c1dcdb814dc502bbf
KLN <karthik.kln@gmail.com>
2021-08-06 16:24:26 +0530

    adding force

--- scripts/terraform-bin-install.sh
+++ scripts/terraform-bin-install.sh
@@ -9,7 +9,7 @@ if [ $status -eq 0 ]; then
   echo "terraform  installed already, Skipping this stage"
   exit 0
   else
-  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip && unzip terraform_0.14.5_linux_amd64.zip && mv terraform /bin/
+  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip && unzip terraform_0.14.5_linux_amd64.zip && mv -f terraform /bin/
   terraform -v
   rm -rf terraform_0.14.5_linux_amd64.zip
   exit  $status
