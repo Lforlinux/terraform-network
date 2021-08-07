d762c3565b0358e6d152b4a8110d5954b92a8aa7
KLN <karthik.kln@gmail.com>
2021-08-07 11:43:41 +0530

    remote backend added

--- SL/backend.tf
+++ SL/backend.tf
@@ -1,1 +1,8 @@
 #https://www.terraform.io/docs/language/settings/backends/s3.html
+terraform {
+  backend "s3" {
+    bucket = "sl-terra-data"
+    key    = "terra-state"
+    region = "us-east-1"
+  }
+}
